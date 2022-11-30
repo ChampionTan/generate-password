8e4fc48b65aaa9129ba31cad5e843ba1e332eb3d
unknown <tannerwlumsden@gmail.com>
2022-11-30 17:17:55 -0500

    added alert

--- generatepassword/script.js
+++ generatepassword/script.js
@@ -2,44 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-var listSpecial = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
-var listLetters = 'abcdefghijklmnopqrstuvwxyz';
-var listNumbers = '0987654321';
-var upLetters = listLetters.toUpperCase();
-var collection = "";
+  var listSpecial = '!#$%&()*+,-./:;<=>?@[]^_`{|}~';
+  var listLetters = 'abcdefghijklmnopqrstuvwxyz';
+  var listNumbers = '0987654321';
+  var upLetters = listLetters.toUpperCase();
+  var collection = "";
 
-var isUppercase = confirm('Would you like to use uppercase?');
-var isLowercase = confirm('Would you like to use lowercase?');
-var isSpecial = confirm('Would you like to use special characters?');
-var isNumber = confirm('Would you like to use numbers?');
-var isPassLength = prompt('How long would you like your password?');
+  var isUppercase = confirm('Would you like to use uppercase?');
+  var isLowercase = confirm('Would you like to use lowercase?');
+  var isSpecial = confirm('Would you like to use special characters?');
+  var isNumber = confirm('Would you like to use numbers?');
+  var isPassLength = prompt('How long would you like your password?');
 
-if (isUppercase) {
-  // USE UPPER CASE LETTERS
-  collection = collection.concat(upLetters);
-}
-
-if (isLowercase) {
-  // USE LOWER CASE LETTERS
-  collection = collection.concat(listLetters);
+  if (isUppercase) {
+    // USE UPPER CASE LETTERS
+    collection = collection.concat(upLetters);
   }
 
-if (isSpecial) {
-  // USE SPECIAL CHARACTER
-  collection = collection.concat(listSpecial);
-}
+  if (isLowercase) {
+    // USE LOWER CASE LETTERS
+    collection = collection.concat(listLetters);
+  }
 
-if (isNumber) {
- // USE A NUMBER
- collection = collection.concat(listNumbers);
-}
+  if (isSpecial) {
+    // USE SPECIAL CHARACTER
+    collection = collection.concat(listSpecial);
+  }
 
-for (isPassLength; isPassLength < 8 || isPassLength > 128;){
-  alert("Please choose a length between 8 and 128");
-  var isPassLength = prompt('How long would you like your password?');
-}
-console.log(collection);
-  return "";
+  if (isNumber) {
+    // USE A NUMBER
+    collection = collection.concat(listNumbers);
+  }
+
+  for (isPassLength; isPassLength < 8 || isPassLength > 128;) {
+    alert("Please choose a length between 8 and 128");
+    var isPassLength = prompt('How long would you like your password?');
+  }
+
+
+
+  if (!isUppercase && !isLowercase && !isSpecial && !isNumber) {
+    alert("Please choose at least one value");
+      
+  }
+  console.log(collection);
+  return Math.floor(Math.random() * collection.length);
 }
 
 // Write password to the #password input
